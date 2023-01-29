2ff847433c8317796cfbdf8dd6634ce8f785b619
eliwelton silva <eliwelton.rc@gmail.com>
2023-01-29 12:41:02 -0300

    Troca de jogador e de turno

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -42,9 +43,18 @@ public class UI {
 				int row = Integer.parseInt(s.substring(1));
 				return new ChessPosition(column, row);
 		}
-		catch(RuntimeException e){}
+		catch(RuntimeException e){
 			throw new InputMismatchException("Erro lendo posicao, valores validos sao de a1 ate h8");
+		}
 	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turno : " + chessMatch.getTurn());
+		System.out.println("Esperando jogador: " + chessMatch.getCurrentPlayer());
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
